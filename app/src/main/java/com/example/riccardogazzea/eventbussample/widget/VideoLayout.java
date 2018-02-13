1b80534b7ef4fcb9fa1ba901abc3b240133b48db
gazz <riccardo.gazzea@officina.cc>
2018-02-13 19:07:23 +0100

    add exoplayer

--- app/src/main/java/com/example/riccardogazzea/eventbussample/widget/VideoLayout.java
+++ app/src/main/java/com/example/riccardogazzea/eventbussample/widget/VideoLayout.java
@@ -1,6 +1,7 @@
 package com.example.riccardogazzea.eventbussample.widget;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -11,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.baseandroid.events.EventDispatcher;
 import com.baseandroid.events.rx.annotations.RxSubscribe;
@@ -19,6 +19,21 @@ import com.example.riccardogazzea.eventbussample.R;
 import com.example.riccardogazzea.eventbussample.events.UiRecyclerStateIdleEvent;
 import com.example.riccardogazzea.eventbussample.events.UiRecyclerStateNotIdleEvent;
 import com.example.riccardogazzea.eventbussample.model.VideoMediaModel;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.Util;
 
 /**
  * Created on 13/02/18.
@@ -29,7 +44,8 @@ public class VideoLayout extends RelativeLayout {
 
     private VideoMediaModel mMediaModel;
 
-    private VideoView mVideoView;
+    private SimpleExoPlayerView mSimpleExoPlayerView;
+    private SimpleExoPlayer mPlayer;
     private ImageView mPlaceholderImageView;
     private TextView mTextTextView;
     private boolean mPlaying = false;
@@ -60,7 +76,20 @@ public class VideoLayout extends RelativeLayout {
         setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getResources().getDimensionPixelSize(R.dimen.listitem_height)));
         setBackgroundColor(ContextCompat.getColor(context, android.R.color.black));
 
-        mVideoView = findViewById(R.id.videolayout_player);
+        // 1. Create a default TrackSelector
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create the player
+        mPlayer =
+                ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+        mPlayer.setPlayWhenReady(true);
+        mSimpleExoPlayerView = findViewById(R.id.videolayout_player);
+        mSimpleExoPlayerView.setPlayer(mPlayer);
+
         mPlaceholderImageView = findViewById(R.id.videolayout_placeholder_image);
         mTextTextView = findViewById(R.id.videolayout_test_text);
     }
@@ -95,6 +124,22 @@ public class VideoLayout extends RelativeLayout {
 
     public void setVideoMediaModel(VideoMediaModel mediaModel) {
         mMediaModel = mediaModel;
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(buildDataSourceFactory(true)).createMediaSource(
+                        Uri.parse(mMediaModel.getUrl()));
+        // Prepare the player with the source.
+        mPlayer.prepare(mediaSource);
+    }
+
+    private DataSource.Factory buildDataSourceFactory(boolean useBandwidthMeter){
+        return buildDataSourceFactory(useBandwidthMeter ? new DefaultBandwidthMeter(): null);
+    }
+
+    private DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter){
+        return new DefaultDataSourceFactory(getContext(), bandwidthMeter, buildHttpDataSourceFactory(bandwidthMeter));
+    }
+
+    public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+        return new DefaultHttpDataSourceFactory(Util.getUserAgent(getContext(), "EventBusSample"), bandwidthMeter);
     }
 
     private boolean isPlaying() {
@@ -103,23 +148,24 @@ public class VideoLayout extends RelativeLayout {
 
     public void play() {
         mPlaying = true;
-        // TODO start video
-        mTextTextView.setText("Playing...");
+        if (mPlayer != null){
+            mPlayer.setPlayWhenReady(true);
+        }
     }
 
     public void pause() {
         // TODO pause video
         if (isPlaying()) {
             mPlaying = false;
-            mTextTextView.setText("Paused");
+            mPlayer.setPlayWhenReady(false);
         }
     }
 
     public void stop() {
-        // TODO stop video and release related resources
         if (isPlaying()) {
             mPlaying = false;
-            mTextTextView.setText("Stopped");
+            mPlayer.stop();
+            mPlayer.release();
         }
     }
 }
